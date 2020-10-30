b7d682fe3ce95ec3c4a56f968dd5b852dad3e471
Yash <yashn49@gmail.com>
2020-10-31 04:42:11 +0530

    Count

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -9,3 +9,18 @@ if [ $isHeads -eq $coinFlip ]
  else
   echo "tails is winner"
 fi
+read -p "enter a number you want loop to run: " num
+count=0
+count1=0
+for ((i=0; i<num; i++))
+do
+ coinFlip=$((RANDOM%2))
+ if [ $coinFlip -eq 0 ]
+ then
+  count=$((count+1))
+ else
+  count1=$((count1+1))
+ fi
+done
+echo "Head has come $count times"
+echo "Tail has come $count1 times"
